ddcd708be4ad9cd345bc4197d008413738366ffa
=dave00galloway <=david+a+galloway@yahoo.co.uk>
2015-02-01 11:21:34 +0000

    added official solution to MyFirstIO

--- LearnYouNode/MyFirstIO/program.js
+++ LearnYouNode/MyFirstIO/program.js
@@ -9,3 +9,15 @@ var string = buffer.toString();
 var arrLines = string.split('\n');
 
 console.log(arrLines.length-1);
+
+//official solution
+//var fs = require('fs')
+//
+//var contents = fs.readFileSync(process.argv[2])
+//var lines = contents.toString().split('\n').length - 1
+//console.log(lines)
+//
+//// note you can avoid the .toString() by passing 'utf8' as the
+//// second argument to readFileSync, then you'll get a String!
+////
+//// fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1

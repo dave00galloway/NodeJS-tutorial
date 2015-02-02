c8164ec96147c30860f4230f3780e1a8a42c0688
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-02-02 23:40:02 +0000

    make it modular done!

--- LearnYouNode/make_it_modular/mymodule.js
+++ LearnYouNode/make_it_modular/mymodule.js
@@ -2,20 +2,17 @@ module.exports = function(dir_path,ext,callback)
 {
 	var fs = require('fs');
     var path = require('path');
-
-    //console.log(dir_path);
-    //console.log(ext);
+    var arrFiltered = undefined;
 
     fs.readdir(dir_path, function (err, list) {
     	if(err)
     		return callback(err);
-      list.forEach(function (file) {
-		//	console.log(file);
-		//	console.log(path.extname(file));    	  
-        if (path.extname(file) === '.' + ext)
-        	//console.log("about to call callback");
-          return callback(null,file);
-      });
+    	arrFiltered = list.filter(function (item){
+			if(path.extname(item) === '.' + ext){				
+				return item;
+			}
+		});
+    	return callback(null,arrFiltered);
     });	
 };
 
--- LearnYouNode/make_it_modular/program.js
+++ LearnYouNode/make_it_modular/program.js
@@ -2,12 +2,49 @@ var mymodule = require('./mymodule.js');
 
 function logResults(err,data){
 	if (err) throw err;
-//	for ( var i = 0; i < data.length; i++) {		
-//	console.log(data[i]);
-//	}
-//	console.log("in function");
-	console.log(data);
+	for ( var i = 0; i < data.length; i++) {		
+		console.log(data[i]);
+	}
 }
-//console.log("about to call module");
+
 mymodule(process.argv[2],process.argv[3],logResults);
-//console.log("called module");
+
+//Your solution to MAKE IT MODULAR passed!
+//
+//Here's the official solution in case you want to compare notes:
+//
+//
+//solution.js:
+//
+//    var filterFn = require('./solution_filter.js')
+//    var dir = process.argv[2]
+//    var filterStr = process.argv[3]
+//    
+//    filterFn(dir, filterStr, function (err, list) {
+//      if (err)
+//        return console.error('There was an error:', err)
+//    
+//      list.forEach(function (file) {
+//        console.log(file)
+//      })
+//    })
+//
+//
+//solution_filter.js:
+//
+//    var fs = require('fs')
+//    var path = require('path')
+//    
+//    module.exports = function (dir, filterStr, callback) {
+//    
+//      fs.readdir(dir, function (err, list) {
+//        if (err)
+//          return callback(err)
+//    
+//        list = list.filter(function (file) {
+//          return path.extname(file) === '.' + filterStr
+//        })
+//    
+//        callback(null, list)
+//      })
+//    }

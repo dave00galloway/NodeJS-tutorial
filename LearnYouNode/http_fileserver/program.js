e14889e9c9b575412eda39d7adf89684b48473f9
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-02-11 11:20:16 +0000

    http_uppercaser done (although some of the validation was bypassed!)

--- LearnYouNode/http_fileserver/program.js
+++ LearnYouNode/http_fileserver/program.js
@@ -1,13 +1,15 @@
-    var http = require('http');
-    var fs = require('fs');
-    var server = http.createServer(function (req, res) {
-      // request handling logic...
-    	var file = fs.createReadStream(process.argv[3]);
-    	file.pipe(res);
-    });
-    server.listen(process.argv[2]);
-    
-    //suprised my solution was allowed since I didn't include a head!
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(req, res) {
+	res.writeHead(200, {
+		'content-type' : 'text/plain'
+	});
+	var file = fs.createReadStream(process.argv[3]);
+	file.pipe(res);
+});
+server.listen(process.argv[2]);
+
+//suprised my solution was allowed since I didn't include a head!
 //    var http = require('http')
 //    var fs = require('fs')
 //    

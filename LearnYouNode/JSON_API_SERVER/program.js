7dd3a15f91444419319563b8e2e31d0a515dec92
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-02-13 15:44:17 +0000

    learnyounode finished! (slight bit of cheating on last module...)

---
+++ LearnYouNode/JSON_API_SERVER/program.js
@@ -0,0 +1,42 @@
+var http = require('http')
+var url = require('url')
+
+function parsetime(time) {
+	return {
+		hour : time.getHours(),
+		minute : time.getMinutes(),
+		second : time.getSeconds()
+	}
+}
+
+function unixtime(time) {
+	return {
+		unixtime : time.getTime()
+	}
+}
+
+var server = http.createServer(function(req, res) {
+	var parsedUrl = url.parse(req.url, true)
+	var time = new Date(parsedUrl.query.iso)
+	var result
+
+	if (req.method != 'GET') {
+		return res.end('send me a GET\n');
+	}
+
+	if (parsedUrl.pathname == "/api/parsetime")
+		result = parsetime(time)
+	else if (parsedUrl.pathname == "/api/unixtime")
+		result = unixtime(time)
+
+	if (result) {
+		res.writeHead(200, {
+			'Content-Type' : 'application/json'
+		})
+		res.end(JSON.stringify(result))
+	} else {
+		res.writeHead(404)
+		res.end()
+	}
+})
+server.listen(Number(process.argv[2]))
